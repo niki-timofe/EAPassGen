3411f4e7249db7d62ce99f2d7fc877cafd245479
Nikita Timofeev <niki-timofe@ya.ru>
2017-02-20 15:01:33 +0300

    Fix runtime

--- smartpassgen/constants.py
+++ smartpassgen/constants.py
@@ -1,5 +1,5 @@
 VOWELS = 'aeiou'
 CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
-SYMBOLS = '.-=.[].().{}.+-.'
+SYMBOLS = '.-=.-.'
 CONV_LETTERS = {'l': '1', 'e': '3', 'o': '0', 'g': '6', 'b': '8'}
 CONV_LETTERS_TO_SYM = {'a': '@', 's': '$'}
--- smartpassgen/generator.py
+++ smartpassgen/generator.py
@@ -1,42 +1,42 @@
 from random import SystemRandom
-import math
-
 from smartpassgen import constants
 
+import math
 
-def gen_syll(length):
-    syll = ''
+
+def gen_syllables(length):
+    syllables = ''
     while True:
-        if len(syll) == 0:
-            syll += SystemRandom().choice(constants.CONSONANTS + constants.VOWELS)
-        elif len(syll) < length:
-            if syll[::-1] in constants.VOWELS:
-                syll += SystemRandom().choice(constants.CONSONANTS + constants.VOWELS)
+        if len(syllables) == 0:
+            syllables += SystemRandom().choice(constants.CONSONANTS + constants.VOWELS)
+        elif len(syllables) < length:
+            if syllables[::-1] in constants.VOWELS:
+                syllables += SystemRandom().choice(constants.CONSONANTS + constants.VOWELS)
             else:
-                syll += SystemRandom().choice(constants.VOWELS)
+                syllables += SystemRandom().choice(constants.VOWELS)
         else:
             break
 
-    return syll
+    return syllables
 
 
-def randomize_capitals(pas, sylls_num):
+def randomize_capitals(pas, syllables_num):
     pas_list = list(pas)
-    for _ in range(SystemRandom().randint(1, sylls_num)):
+    for _ in range(SystemRandom().randint(1, syllables_num)):
         cur_cap = SystemRandom().randint(0, len(pas) - 1)
         pas_list[cur_cap] = pas[cur_cap].upper()
     return ''.join(pas_list)
 
 
-def add_some_numbers(pas, sylls_num):
+def add_some_numbers(pas, syllables_num):
     pas_list = list(pas)
 
-    for _ in range(SystemRandom().randint(1, sylls_num)):
+    for _ in range(SystemRandom().randint(1, syllables_num)):
         cur_num = SystemRandom().randint(0, len(pas) - 1)
         if letter_to_number(pas_list[cur_num]):
             pas_list[cur_num] = letter_to_number(pas_list[cur_num])
 
-    # for _ in range(SystemRandom().randint(1, sylls_num / 2) - counter):
+    # for _ in range(SystemRandom().randint(1, syllables_num / 2) - counter):
     #     cur_num = SystemRandom().randint(0, len(pas) - 1)
     #     pas_list[cur_num] = str(SystemRandom().randint(0, 9))
     return ''.join(pas_list)
@@ -56,32 +56,32 @@ def letter_to_symbols(letter):
         return False
 
 
-def add_some_symbols(pas, sylls_num):
+def add_some_symbols(pas, syllables_num):
     pas_list = list(pas)
 
-    for _ in range(SystemRandom().randint(1, sylls_num)):
+    for _ in range(SystemRandom().randint(1, syllables_num)):
         cur_num = SystemRandom().randint(0, len(pas) - 1)
         if letter_to_symbols(pas_list[cur_num]):
-            pas_list[cur_num] = letter_to_number(pas_list[cur_num])
+            pas_list[cur_num] = letter_to_symbols(pas_list[cur_num])
 
-    # for _ in range(SystemRandom().randint(1, math.ceil(sylls_num / 4))):
-    #     cur_sym = SystemRandom().randint(0, len(pas) - 1)
-    #     pas_list.insert(cur_sym, SystemRandom().choice(constants.SYMBOLS))
+    for _ in range(SystemRandom().randint(1, math.ceil(syllables_num / 4))):
+        cur_sym = SystemRandom().randint(0, len(pas) - 1)
+        pas_list.insert(cur_sym, SystemRandom().choice(constants.SYMBOLS))
     return ''.join(pas_list)
 
 
-def create_pass(sylls_num, with_caps=False, with_nums=False, with_symbs=False):
+def create_pass(syllables_num, with_caps=False, with_nums=False, with_symbols=False):
     pas = ''
-    for _ in range(sylls_num):
-        pas += gen_syll(SystemRandom().randint(1, 3))
-    pas = randomize_capitals(pas, sylls_num) if with_caps else pas
-    pas = add_some_numbers(pas, sylls_num) if with_nums else pas
-    pas = add_some_symbols(pas, sylls_num) if with_symbs else pas
+    for _ in range(syllables_num):
+        pas += gen_syllables(SystemRandom().randint(1, 3))
+    pas = randomize_capitals(pas, syllables_num) if with_caps else pas
+    pas = add_some_numbers(pas, syllables_num) if with_nums else pas
+    pas = add_some_symbols(pas, syllables_num) if with_symbols else pas
     return pas
 
 
-    # passes_number = int(raw_input('Number of passes to generate: '))
-    # sylls_number = int(raw_input('Number of syllables in pass: '))
+passes_number = int(raw_input('Number of passes to generate: '))
+syllables_number = int(raw_input('Number of syllables in pass: '))
 
-    # for _ in range(passes_number):
-    # print create_pass(int(sylls_number))
+for _ in range(passes_number):
+    print create_pass(int(syllables_number), True, True, True)
